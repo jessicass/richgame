3f13cb7308cdd3d15eace77d2bcf823e9dc4b0a1
Bowen <huangbowen521@gmail.com>
2013-03-17 20:45:34 +0800

    bowen create Terrain.java

--- src/main/java/thoughtworks/publicPlace/MagicRoom.java
+++ src/main/java/thoughtworks/publicPlace/MagicRoom.java
@@ -8,58 +8,9 @@ import thoughtworks.players.Player;
 import thoughtworks.tools.Block;
 import thoughtworks.tools.Bomb;
 
-public class MagicRoom implements MapObject {
+public class MagicRoom extends Terrain implements MapObject {
 	public static final String symbol = "M";
-	private boolean hasBlock;
-	private boolean hasBomb;
-	private int position;
-	
 	public MagicRoom(int position) {
-		this.position = position;
+        super(position);
 	}
-
-	public String getSymbol(ArrayList<Player> players){
-		for(Player player: players){
-			if(player.getPosition() == position){
-				return player.getShortName();
-			}
-		}
-		if(hasBlock){
-			return Block.symbol;
-		}
-		if(hasBomb){
-			return Bomb.symbol;
-		}
-		return symbol;
-	}
-	
-	public int getPosition(){
-		return position;
-	}
-	
-	public void setBlock(){
-		hasBlock = true;
-	}
-	
-	public void resetBlock(){
-		hasBlock = false;
-	}
-	
-	public boolean hasBlock(){
-		return hasBlock;
-	}
-	
-	public void setBomb(){
-		hasBomb = true;
-	}
-	
-	public void resetBomb(){
-		hasBomb = false;
-	}
-	
-	public boolean hasBomb(){
-		return hasBomb;
-	}
-
-	public void playerPassOnHere(Player player, Game game) {}
 }
---
+++ src/main/java/thoughtworks/publicPlace/Terrain.java
@@ -0,0 +1,67 @@
+package thoughtworks.publicPlace;
+
+import thoughtworks.Game;
+import thoughtworks.MapObject;
+import thoughtworks.players.Player;
+import thoughtworks.tools.Block;
+import thoughtworks.tools.Bomb;
+
+import java.util.ArrayList;
+
+public class Terrain implements MapObject {
+    public static final String symbol = "";
+    public boolean hasBlock;
+    public boolean hasBomb;
+    public int position;
+
+    public Terrain(int position) {
+        this.position = position;
+    }
+
+    public String getSymbol(ArrayList<Player> players) {
+        for (Player player : players) {
+            if (player.getPosition() == position) {
+                return player.getShortName();
+            }
+        }
+        if (hasBlock) {
+            return Block.symbol;
+        }
+        if (hasBomb) {
+            return Bomb.symbol;
+        }
+        return symbol;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setBlock() {
+        hasBlock = true;
+    }
+
+    public void resetBlock() {
+        hasBlock = false;
+    }
+
+    public boolean hasBlock() {
+        return hasBlock;
+    }
+
+    public void setBomb() {
+        hasBomb = true;
+    }
+
+    public void resetBomb() {
+        hasBomb = false;
+    }
+
+    public boolean hasBomb() {
+        return hasBomb;
+    }
+
+    public void playerPassOnHere(Player player, Game game) {
+
+    }
+}
